61837f4ae2accb2fc3bfdc7546020b9750ee289b
Kirkulese <44822130+Kirkulese@users.noreply.github.com>
2023-01-07 10:28:47 -0800

    admin set up for categories and products

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . models import Category, Product
+
+# functions to register admin site
+# admin.site.register(Category)
+
+# admin.site.register(Product)
+
+#class to register admin site can use prepopulated fields
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
