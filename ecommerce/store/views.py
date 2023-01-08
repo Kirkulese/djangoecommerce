da0fa76c24a50213b5206d613a0901d8a8f1c3c3
Kirkulese <44822130+Kirkulese@users.noreply.github.com>
2023-01-08 11:31:21 -0800

    added categories to context proccessing

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 
 def store(request):
     #because we have our template folder nested under store we need to add that path here
     return render(request, 'store/store.html')
+
+
+def categories(request):
+    #get all categories from the model
+    all_categories = Category.objects.all()
+    
+
+    return {'all_categories': all_categories}
+
