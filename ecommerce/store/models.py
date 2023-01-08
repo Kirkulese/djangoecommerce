61630273db37b03162d178c6a748116a0539c072
Kirkulese <44822130+Kirkulese@users.noreply.github.com>
2023-01-08 11:19:58 -0800

    linked products to category by foreign key and migrated

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -17,6 +17,9 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    #foreign key to link product to category
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE, null=True)
+
     title = models.CharField(max_length=250)
     brand = models.CharField(max_length=250, default='none')
     description = models.TextField(blank=True)
